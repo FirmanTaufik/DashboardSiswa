359c309e53aafa25cf1ef631d22e99cbeb1492dd
FirmanTaufik <fkopitezz@gmail.com>
2023-03-01 20:05:42 +0700

    change fragment

--- app/src/main/java/com/app/dashboardsiswa/activity/MainActivity.kt
+++ app/src/main/java/com/app/dashboardsiswa/activity/MainActivity.kt
@@ -2,16 +2,27 @@ package com.app.dashboardsiswa.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.forEach
 import androidx.core.view.get
+import androidx.fragment.app.Fragment
 import com.app.dashboardsiswa.R
 import com.app.dashboardsiswa.databinding.ActivityMainBinding
 import com.app.dashboardsiswa.databinding.HeaderMenuBinding
+import com.app.dashboardsiswa.feature.agenda.AgendaFragment
+import com.app.dashboardsiswa.feature.daftar_ulang.DaftarUlangFragment
+import com.app.dashboardsiswa.feature.dashboard.DashboardFragment
+import com.app.dashboardsiswa.feature.perpustakaan.PerpustakaanFragment
+import com.app.dashboardsiswa.feature.profile.ProfileFragment
+import com.app.dashboardsiswa.feature.rekam_data.*
+import com.app.dashboardsiswa.feature.satgas.*
+import com.app.dashboardsiswa.feature.ukgs.UkgsFragment
 import com.app.dashboardsiswa.helper.Utils
 
 
@@ -32,7 +43,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         toggle.syncState()
         binding.navView.bringToFront()
         initHeader()
-
+        setContentFragment( DashboardFragment())
 
     }
 
@@ -50,28 +61,75 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     override fun onClick(p0: View) {
         Log.d(TAG, "onClick: " + p0.id)
         when (p0.id) {
-            R.id.dasboard -> Utils.showToastInfo(this, "dasboard")
-            R.id.profile -> Utils.showToastInfo(this, "profile")
-            R.id.agenda -> Utils.showToastInfo(this, "agenda")
+            R.id.dasboard -> setContentFragment( DashboardFragment())
+            R.id.profile -> setContentFragment( ProfileFragment())
+            R.id.agenda -> setContentFragment( AgendaFragment())
             R.id.rekam -> {
                 if (header.expandableLayout1.isExpanded)
                     header.expandableLayout1.collapse(true)
                 else header.expandableLayout1.expand(true)
                  header.linearSub1[0].setOnClickListener {
-                     Utils.showToastInfo(this, "rekam 1")
+                     setContentFragment( EkstrakurikulerFragment())
                  }
                 header.linearSub1[1].setOnClickListener {
-                    Utils.showToastInfo(this, "rekam 2")
+                    setContentFragment( PengembanganKerohanianFragment())
+                }
+                header.linearSub1[2].setOnClickListener {
+                    setContentFragment( PengembanganKarakterFragment())
+                }
+                header.linearSub1[3].setOnClickListener {
+                    setContentFragment( KegiatanKemasyarakatanFragment())
+                }
+                header.linearSub1[4].setOnClickListener {
+                    setContentFragment( KegiatanSekolahFragment())
+                }
+                header.linearSub1[5].setOnClickListener {
+                    setContentFragment( KepengurusanFragment())
+                }
+                header.linearSub1[6].setOnClickListener {
+                    setContentFragment( PrestasiFragment())
+                }
+                header.linearSub1[7].setOnClickListener {
+                    setContentFragment( ProyekFragment())
+                }
+                header.linearSub1[8].setOnClickListener {
+                    setContentFragment( KegiatanSekolahFragment())
+                }
+                header.linearSub1[9].setOnClickListener {
+                    setContentFragment( RekapitulasiFragment())
                 }
             }
-            R.id.perpustakaan -> Utils.showToastInfo(this, "perpustakaan")
+            R.id.perpustakaan -> setContentFragment( PerpustakaanFragment())
             R.id.satgas -> {
                 if (header.expandableLayout2.isExpanded)
                     header.expandableLayout2.collapse(true)
                 else header.expandableLayout2.expand(true)
+
+                header.linearSub2[0].setOnClickListener {
+                    setContentFragment( SatgasDashboardFragment())
+                }
+                header.linearSub2[1].setOnClickListener {
+                    setContentFragment( SatgastTestFragment())
+                }
+                header.linearSub2[2].setOnClickListener {
+                    setContentFragment( SatgasVaksinasiFragment())
+                }
+                header.linearSub2[3].setOnClickListener {
+                    setContentFragment( SatgasQRFragment())
+                }
+                header.linearSub2[4].setOnClickListener {
+                    setContentFragment( PersiapanPendataanVaksinFragment())
+                }
             }
-            R.id.daftar -> Utils.showToastInfo(this, "daftar")
-            R.id.ukgs -> Utils.showToastInfo(this, "ukgs")
+            R.id.daftar -> setContentFragment( DaftarUlangFragment())
+            R.id.ukgs -> setContentFragment( UkgsFragment())
         }
+
+
+    }
+
+    private fun setContentFragment(fragment: Fragment ) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame,fragment).commit()
+        binding.drawerLayout.closeDrawer(GravityCompat.START, true)
     }
 }
--- app/src/main/java/com/app/dashboardsiswa/feature/satgas/SatgasVaksinasiFragment.kt
+++ app/src/main/java/com/app/dashboardsiswa/feature/satgas/SatgasVaksinasiFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.dashboardsiswa.R
+import com.app.dashboardsiswa.helper.Utils
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +36,7 @@ class SatgasVaksinasiFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Utils.showToastInfo(requireContext(), "SatgasVaksinasiFragment")
         return inflater.inflate(R.layout.fragment_satgas_vaksinasi, container, false)
     }
 
